5fbfaa79e8bfd5510bd591e7f7b87648b647c1b0
Apriansyah R <65652874+apriansyahrs@users.noreply.github.com>
2022-06-07 15:11:00 +0700

    Update perancangan.js

--- src/pages/perancangan.js
+++ src/pages/perancangan.js
@@ -8,13 +8,7 @@ const Perancangan = () => {
           <div className="flex flex-col">
             <div className="space-y-4">
               <h1 className="text-2xl font-bold">Perancangan</h1>
-              <iframe
-                style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
-                width={1024}
-                height={450}
-                src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FB7z4FH44QQSZnj8mOhU9FF%2FAkses-Digital---Design-System%3Fnode-id%3D0%253A1"
-                allowFullScreen
-              />
+               <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width={1024} height={450} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FB7z4FH44QQSZnj8mOhU9FF%2FAkses-Digital---Design-System%3Fnode-id%3D0%253A1" allowFullScreen />
             </div>
           </div>
         </div>
