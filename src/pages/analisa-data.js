25887980e99b2552167e53e843789f7f83601879
Apriansyah R <65652874+apriansyahrs@users.noreply.github.com>
2022-06-07 15:12:54 +0700

    Update analisa-data.js

--- src/pages/analisa-data.js
+++ src/pages/analisa-data.js
@@ -8,13 +8,7 @@ const AnalisaData = () => {
           <div className="flex flex-col">
             <div className="space-y-4">
               <h1 className="text-2xl font-bold">Analisa Data</h1>
-              <iframe
-                style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
-                width={1024}
-                height={450}
-                src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FGyUFp7jRPBv4uNXCTiEYIg%2FUntitled%3Fnode-id%3D0%253A1"
-                allowFullScreen
-              />
+              <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width={1024} height={450} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FGyUFp7jRPBv4uNXCTiEYIg%2FAnalisa-Data---Akses-Digital-%255BDS%255D" allowFullScreen />
             </div>
           </div>
         </div>
